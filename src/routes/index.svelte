5d8a7c3b3109556cdcfcefcf545a657f0ed89484
Mike Jerome <m@j4e.name>
2022-02-20 10:50:36 +0000

    Format time since

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,38 @@
 <script>
 	export let build;
-    const buildDate = build.date;
+	const buildDate = build.date;
 
 	let timeNow = Date.now();
-    setInterval(updateTime, 1000);
+	setInterval(updateTime, 1000);
 	function updateTime() {
-		console.log('updateTime ran');
 		timeNow = Date.now();
+	}
+
+	let timeSinceBuild = timeAgo(buildDate);
+	setInterval(updateTimeSinceBuild, 1000);
+	function updateTimeSinceBuild() {
+		timeSinceBuild = timeAgo(buildDate);
+	}
+
+	function timeAgo(input) {
+		const date = input instanceof Date ? input : new Date(input);
+		const formatter = new Intl.RelativeTimeFormat('en');
+		const ranges = {
+			years: 3600 * 24 * 365,
+			months: 3600 * 24 * 30,
+			weeks: 3600 * 24 * 7,
+			days: 3600 * 24,
+			hours: 3600,
+			minutes: 60,
+			seconds: 1
+		};
+		const secondsElapsed = (date.getTime() - Date.now()) / 1000;
+		for (let key in ranges) {
+			if (ranges[key] < Math.abs(secondsElapsed)) {
+				const delta = secondsElapsed / ranges[key];
+				return formatter.format(Math.round(delta), key);
+			}
+		}
 	}
 </script>
 
@@ -14,3 +40,4 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <p>Time now: {timeNow}</p>
 <p>Build date: {buildDate}</p>
+<p>This page was built {timeSinceBuild}.</p>
