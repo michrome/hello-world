20bf2fba84b697af3df4836f2ed55f819ed8f85d
Mike Jerome <m@j4e.name>
2022-02-20 11:43:31 +0000

    Add tests.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,13 +8,24 @@
 		timeNow = Date.now();
 	}
 
+	//console.log(buildDate);
 	let timeSinceBuild = timeAgo(buildDate);
+	//console.log(timeSinceBuild);
 	setInterval(updateTimeSinceBuild, 1000);
 	function updateTimeSinceBuild() {
 		timeSinceBuild = timeAgo(buildDate);
 	}
 
-	function timeAgo(input) {
+	// const date2 = new Date('1995-12-17T03:24:00');
+	const no_time = timeAgo(new Date('2022-02-20T11:20:00'), new Date('2022-02-20T11:20:00'));
+	const one_sec = timeAgo(new Date('2022-02-20T11:20:00'), new Date('2022-02-20T11:20:01'));
+	const five_nine_sec = timeAgo(new Date('2022-02-20T11:20:00'), new Date('2022-02-20T11:20:59'));
+	const six_o_sec = timeAgo(new Date('2022-02-20T11:20:00'), new Date('2022-02-20T11:21:00'));
+    const five_nine_min = timeAgo(new Date('2022-02-20T11:20:00'), new Date('2022-02-20T12:19:00'));
+    const five_nine_min_five_nine = timeAgo(new Date('2022-02-20T11:20:00'), new Date('2022-02-20T12:19:59'));
+    const one_hour = timeAgo(new Date('2022-02-20T11:20:00'), new Date('2022-02-20T12:20:00'));
+
+	function timeAgo(input, now = Date.now()) {
 		const date = input instanceof Date ? input : new Date(input);
 		const formatter = new Intl.RelativeTimeFormat('en');
 		const ranges = {
@@ -26,9 +37,12 @@
 			minutes: 60,
 			seconds: 1
 		};
-		const secondsElapsed = (date.getTime() - Date.now()) / 1000;
+		const secondsElapsed = (date - now) / 1000;
 		for (let key in ranges) {
-			if (ranges[key] < Math.abs(secondsElapsed)) {
+			if (Math.abs(secondsElapsed) < 1) {
+				return "***NOW***";
+			}
+			if (ranges[key] <= Math.abs(secondsElapsed)) {
 				const delta = secondsElapsed / ranges[key];
 				return formatter.format(Math.round(delta), key);
 			}
@@ -41,3 +55,10 @@
 <p>Time now: {timeNow}</p>
 <p>Build date: {buildDate}</p>
 <p>This page was built {timeSinceBuild}.</p>
+<p>no_time: {no_time}</p>
+<p>one_sec: {one_sec}</p>
+<p>five_nine_sec: {five_nine_sec}</p>
+<p>six_o_sec: {six_o_sec}</p>
+<p>five_nine_min: {five_nine_min}</p>
+<p>five_nine_min_five_nine: {five_nine_min_five_nine}</p>
+<p>one_hour: {one_hour}</p>
