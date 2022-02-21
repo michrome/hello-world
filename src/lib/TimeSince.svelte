43c824c18fc6a93c430c5658996b4f678aae73a9
Mike Jerome <m@j4e.name>
2022-02-21 23:19:41 +0000

    Show buildtime and elapsed time.

---
+++ src/lib/TimeSince.svelte
@@ -0,0 +1,43 @@
+<script>
+	import { onMount } from 'svelte';
+	export let time;
+	let now = false;
+
+	$: secondsElapsed = now ? Math.round((time - now) / 1000) : 0;
+
+	onMount(() => {
+		setNow();
+		const interval = setInterval(() => {
+			setNow();
+		}, 1000);
+		return () => clearInterval(interval);
+	});
+
+	function friendlySeconds(secondsElapsed) {
+		const formatter = new Intl.RelativeTimeFormat('en-GB', { numeric: 'auto' });
+		if (secondsElapsed === 0) return formatter.format(0, 'seconds');
+		const ranges = {
+			years: 3600 * 24 * 365,
+			months: 3600 * 24 * 30,
+			weeks: 3600 * 24 * 7,
+			days: 3600 * 24,
+			hours: 3600,
+			minutes: 60,
+			seconds: 1
+		};
+		for (let key in ranges) {
+			if (ranges[key] <= Math.abs(secondsElapsed)) {
+				const delta = secondsElapsed / ranges[key];
+				return formatter.format(Math.round(delta), key);
+			}
+		}
+	}
+
+	function setNow() {
+		now = Date.now();
+	}
+</script>
+
+{#if now}
+	{@html ' '}({friendlySeconds(secondsElapsed)})
+{/if}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,65 +1,16 @@
 <script>
+	import TimeSince from '$lib/TimeSince.svelte';
 	export let build;
-	const buildDate = build.date;
 
-	let timeNow = Date.now();
-	setInterval(updateTime, 1000);
-	function updateTime() {
-		timeNow = Date.now();
-	}
-
-	//console.log(buildDate);
-	let timeSinceBuild = timeAgo(buildDate);
-	//console.log(timeSinceBuild);
-	function updateTimeSinceBuild() {
-		timeSinceBuild = timeAgo(buildDate);
-	}
-    updateTimeSinceBuild();
-	setInterval(updateTimeSinceBuild, 1000);
-
-	// const date2 = new Date('1995-12-17T03:24:00');
-	const no_time = timeAgo(new Date('2022-02-20T11:20:00'), new Date('2022-02-20T11:20:00'));
-	const one_sec = timeAgo(new Date('2022-02-20T11:20:00'), new Date('2022-02-20T11:20:01'));
-	const five_nine_sec = timeAgo(new Date('2022-02-20T11:20:00'), new Date('2022-02-20T11:20:59'));
-	const six_o_sec = timeAgo(new Date('2022-02-20T11:20:00'), new Date('2022-02-20T11:21:00'));
-    const five_nine_min = timeAgo(new Date('2022-02-20T11:20:00'), new Date('2022-02-20T12:19:00'));
-    const five_nine_min_five_nine = timeAgo(new Date('2022-02-20T11:20:00'), new Date('2022-02-20T12:19:59'));
-    const one_hour = timeAgo(new Date('2022-02-20T11:20:00'), new Date('2022-02-20T12:20:00'));
-
-	function timeAgo(input, now = Date.now()) {
-		const date = input instanceof Date ? input : new Date(input);
-		const formatter = new Intl.RelativeTimeFormat('en');
-		const ranges = {
-			years: 3600 * 24 * 365,
-			months: 3600 * 24 * 30,
-			weeks: 3600 * 24 * 7,
-			days: 3600 * 24,
-			hours: 3600,
-			minutes: 60,
-			seconds: 1
-		};
-		const secondsElapsed = (date - now) / 1000;
-		for (let key in ranges) {
-			if (Math.abs(secondsElapsed) < 1) {
-				return "***NOW***";
-			}
-			if (ranges[key] <= Math.abs(secondsElapsed)) {
-				const delta = secondsElapsed / ranges[key];
-				return formatter.format(Math.round(delta), key);
-			}
-		}
-	}
+	const buildDate = new Date(build.date);
+	const formattedBuildDate = buildDate.toLocaleDateString('en-GB', { dateStyle: 'long' });
+	const formattedBuildTime = buildDate.toLocaleTimeString('en-GB', { timeStyle: 'medium' });
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p>Time now: {timeNow}</p>
-<p>Build date: {buildDate}</p>
-<p>This page was built {timeSinceBuild}.</p>
-<p>no_time: {no_time}</p>
-<p>one_sec: {one_sec}</p>
-<p>five_nine_sec: {five_nine_sec}</p>
-<p>six_o_sec: {six_o_sec}</p>
-<p>five_nine_min: {five_nine_min}</p>
-<p>five_nine_min_five_nine: {five_nine_min_five_nine}</p>
-<p>one_hour: {one_hour}</p>
+<p>
+	This page was built on <time datetime={buildDate.toISOString()}
+		>{formattedBuildDate} at {formattedBuildTime}<TimeSince time={buildDate} /></time
+	>.
+</p>
