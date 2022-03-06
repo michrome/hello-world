8207f8e2b7238aeee1f479dc8451dae476f8df30
Mike Jerome <m@j4e.name>
2022-03-06 22:46:47 +0000

    Use env during build

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,1 +1,1 @@
-export const CANONICAL_BASE_URL = 'https://aws.hello-yoga.co.uk';
+export const CANONICAL_BASE_URL = process.env.CANONICAL_BASE_URL;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,22 @@
 import { S3Client, ListObjectsV2Command } from '@aws-sdk/client-s3';
 import { canonicalUrl } from '$lib/helpers';
 
-const REGION = 'us-east-1';
-const BUCKET_NAME = 'hello-yoga.co.uk';
-const ORIGIN_PATH = 'www';
-const IMAGES_FOLDER = 'images';
-const IMAGES_PREFIX = [ORIGIN_PATH, IMAGES_FOLDER].join('/');
+const AWS_REGION = process.env.AWS_REGION;
+const ASSET_BUCKET_NAME = process.env.ASSET_BUCKET_NAME;
+const ASSET_FOLDER = process.env.ASSET_FOLDER;
+const IMAGE_FOLDER = process.env.IMAGE_FOLDER;
+
 export async function get() {
 	const build = { date: Date.now() };
 
 	const imageUrls = [];
-	const client = new S3Client({ region: 'us-east-1' });
-	const command = new ListObjectsV2Command({ Bucket: BUCKET_NAME, Prefix: IMAGES_PREFIX });
+	const client = new S3Client({ region: AWS_REGION });
+	const command = new ListObjectsV2Command({ Bucket: ASSET_BUCKET_NAME, Prefix: IMAGE_FOLDER });
 	const response = await client.send(command);
 	let contents = response.Contents;
 	contents?.sort((a, b) => b.LastModified - a.LastModified);
 	contents?.forEach((item) => {
-		if (item.Size > 0) imageUrls.push(canonicalUrl(item.Key.slice(ORIGIN_PATH.length)));
+		if (item.Size > 0) imageUrls.push(canonicalUrl(item.Key.slice(ASSET_FOLDER.length)));
 	});
 
 	return {
